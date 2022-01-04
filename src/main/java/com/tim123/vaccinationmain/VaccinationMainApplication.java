4aa3476a73ea383025026b60eaca2eaa24a97593
Luka Kureljušić <lkureljusic@ymail.com>
2022-01-04 21:50:44 +0100

    model generated

---
+++ src/main/java/com/tim123/vaccinationmain/VaccinationMainApplication.java
@@ -0,0 +1,13 @@
+package com.tim123.vaccinationmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim123/vaccinationmain/VaccinationMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim123.vaccinationmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
