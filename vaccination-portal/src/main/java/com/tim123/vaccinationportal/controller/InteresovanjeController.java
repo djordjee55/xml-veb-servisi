d6a1cddab99069c50a114f9a6efc2912f4fd30d6
deksa99 <deja99@live.com>
2022-01-12 18:43:19 +0100

    Interesovanje Controller Added

---
+++ vaccination-portal/src/main/java/com/tim123/vaccinationportal/controller/InteresovanjeController.java
@@ -0,0 +1,29 @@
+package com.tim123.vaccinationportal.controller;
+
+import com.tim123.vaccinationportal.model.interesovanje.Interesovanje;
+import com.tim123.vaccinationportal.service.InteresovanjeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/interesovanje")
+@RequiredArgsConstructor
+public class InteresovanjeController {
+
+    private final InteresovanjeService interesovanjeService;
+
+    @PostMapping(value = "/interesovanje", consumes = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<String> dodajInteresovanje(@RequestBody Interesovanje interesovanje) {
+        interesovanjeService.dodajInteresovanje(interesovanje);
+        return ResponseEntity.ok("Interesovanje uspesno dodato");
+    }
+
+    @GetMapping(value = "/interesovanje/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Interesovanje> dobaviInteresovanje(@PathVariable String id) {
+        var interesovanje = interesovanjeService.dobaviInteresovanje(id);
+        return ResponseEntity.ok(interesovanje);
+    }
+
+}
