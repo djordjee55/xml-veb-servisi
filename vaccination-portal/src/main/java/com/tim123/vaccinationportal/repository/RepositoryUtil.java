3d1a0c7b9e203bf4b9a22c6b7dda8109b8a69aab
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-01-12 17:56:37 +0100

    RepositoryUtil added

---
+++ vaccination-portal/src/main/java/com/tim123/vaccinationportal/repository/RepositoryUtil.java
@@ -0,0 +1,153 @@
+package com.tim123.vaccinationportal.repository;
+
+import com.tim123.vaccinationportal.util.AuthenticationUtilities;
+import com.tim123.vaccinationportal.util.MarshallUnmarshallFactory;
+import org.exist.xmldb.EXistResource;
+import org.springframework.stereotype.Component;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.io.StringWriter;
+
+@Component
+public class RepositoryUtil {
+
+    public String convertToXmlString(Object object, Class<?> objectClass) throws JAXBException {
+        var marshaller = MarshallUnmarshallFactory.getMarshaller(objectClass);
+        var stringWriter = new StringWriter();
+        marshaller.marshal(object, stringWriter);
+        return stringWriter.toString();
+    }
+
+    public void save(String collectionId, String documentId, String xmlData) throws Exception {
+        AuthenticationUtilities.ConnectionProperties props = AuthenticationUtilities.loadProperties();
+
+        // initialize database driver
+        Class<?> cl = Class.forName(props.driver);
+
+        // encapsulation of the database driver functionality
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+
+        // entry point for the API which enables you to get the Collection reference
+        DatabaseManager.registerDatabase(database);
+        // a collection of Resources stored within an XML database
+        Collection col = null;
+        XMLResource res = null;
+
+        try {
+            col = getOrCreateCollection(collectionId, props);
+            res = (XMLResource) col.createResource( documentId + ".xml", XMLResource.RESOURCE_TYPE);
+            res.setContent(xmlData);
+            col.storeResource(res);
+        } finally {
+            if (res != null) {
+                try {
+                    ((EXistResource) res).freeResources();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Collection getOrCreateCollection(String collectionUri,
+                                             AuthenticationUtilities.ConnectionProperties props) throws XMLDBException {
+        return getOrCreateCollection(collectionUri, 0, props);
+    }
+
+    private Collection getOrCreateCollection(String collectionUri,
+                                             int pathSegmentOffset,
+                                             AuthenticationUtilities.ConnectionProperties props) throws XMLDBException {
+        Collection col = DatabaseManager
+                .getCollection(props.uri + collectionUri, props.user, props.password);
+
+        // create the collection if it does not exist
+        if (col == null) {
+
+            if (collectionUri.startsWith("/")) {
+                collectionUri = collectionUri.substring(1);
+            }
+
+            String[] pathSegments = collectionUri.split("/");
+
+            if (pathSegments.length > 0) {
+                StringBuilder path = new StringBuilder();
+
+                for (int i = 0; i <= pathSegmentOffset; i++) {
+                    path.append("/").append(pathSegments[i]);
+                }
+
+                Collection startCol = DatabaseManager
+                        .getCollection(props.uri + path, props.user, props.password);
+
+                if (startCol == null) {
+                    String parentPath = path.substring(0, path.lastIndexOf("/"));
+                    Collection parentCol = DatabaseManager
+                            .getCollection(props.uri + parentPath, props.user, props.password);
+
+                    CollectionManagementService mgt = (CollectionManagementService) parentCol
+                            .getService("CollectionManagementService", "1.0");
+
+                    col = mgt.createCollection(pathSegments[pathSegmentOffset]);
+
+                    col.close();
+                    parentCol.close();
+                } else {
+                    startCol.close();
+                }
+            }
+            return getOrCreateCollection(collectionUri, ++pathSegmentOffset, props);
+        } else {
+            return col;
+        }
+    }
+
+    public String findByDocumentId(String collection, String documentId) throws IOException, ClassNotFoundException, XMLDBException, IllegalAccessException, InstantiationException {
+        AuthenticationUtilities.ConnectionProperties props = AuthenticationUtilities.loadProperties();
+        Collection col = null;
+
+        Class<?> cl = Class.forName(props.driver);
+
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+
+        DatabaseManager.registerDatabase(database);
+
+        try {
+            col = DatabaseManager.getCollection(props.uri + "/" + collection);
+
+            if (col == null) {
+                return null;
+            }
+            var res = (XMLResource)col.getResource(documentId + ".xml");
+            if (res != null) {
+                return (String) res.getContent();
+            }
+        } catch(Exception e) {
+          return "";
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+        }
+        return "";
+    }
+}
---
+++ vaccination-portal/src/main/java/com/tim123/vaccinationportal/util/AuthenticationUtilities.java
@@ -0,0 +1,51 @@
+package com.tim123.vaccinationportal.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AuthenticationUtilities {
+
+    private static final String connectionUri = "xmldb:exist://%1$s:%2$s/exist/xmlrpc";
+
+    static public class ConnectionProperties {
+
+        public String host;
+        public int port = -1;
+        public String user;
+        public String password;
+        public String driver;
+        public String uri;
+
+        public ConnectionProperties(Properties props) {
+            super();
+
+            user = props.getProperty("conn.user").trim();
+            password = props.getProperty("conn.password").trim();
+
+            host = props.getProperty("conn.host").trim();
+            port = Integer.parseInt(props.getProperty("conn.port"));
+
+            uri = String.format(connectionUri, host, port);
+
+            driver = props.getProperty("conn.driver").trim();
+        }
+    }
+
+    public static ConnectionProperties loadProperties() throws IOException {
+        String propsName = "exist.properties";
+
+        InputStream propsStream = openStream(propsName);
+        if (propsStream == null)
+            throw new IOException("Could not read properties " + propsName);
+
+        Properties props = new Properties();
+        props.load(propsStream);
+
+        return new ConnectionProperties(props);
+    }
+
+    public static InputStream openStream(String fileName) throws IOException {
+        return AuthenticationUtilities.class.getClassLoader().getResourceAsStream(fileName);
+    }
+}
