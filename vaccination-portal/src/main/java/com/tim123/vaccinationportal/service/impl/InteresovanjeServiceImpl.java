68dab35825df8eeaa15795d7f1e899c1f4b5f1d7
deksa99 <deja99@live.com>
2022-01-12 18:40:48 +0100

    Interesovanje service added

--- vaccination-portal/src/main/java/com/tim123/vaccinationportal/service/impl/InteresovanjeServiceImpl.java
+++ vaccination-portal/src/main/java/com/tim123/vaccinationportal/service/impl/InteresovanjeServiceImpl.java
@@ -2,17 +2,46 @@ package com.tim123.vaccinationportal.service.impl;
 
 import com.tim123.vaccinationportal.model.interesovanje.Interesovanje;
 import com.tim123.vaccinationportal.repository.CRUDRepository;
+import com.tim123.vaccinationportal.repository.InteresovanjeRepository;
 import com.tim123.vaccinationportal.service.InteresovanjeService;
+import com.tim123.vaccinationportal.service.RDFService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import static com.tim123.vaccinationportal.util.Constants.interesovanjePath;
 
 @Service
 @RequiredArgsConstructor
 public class InteresovanjeServiceImpl extends CRUDServiceImpl<Interesovanje> implements InteresovanjeService {
 
+    private final InteresovanjeRepository interesovanjeRepository;
+    private final RDFService rdfService;
+
     @Override
     protected CRUDRepository<Interesovanje> getRepository() {
-        return null;
+        return interesovanjeRepository;
+    }
+
+    @Override
+    public Interesovanje dodajInteresovanje(Interesovanje interesovanje) {
+        try {
+            var i = this.save(interesovanje);
+            rdfService.extractMetadata(interesovanje, Interesovanje.class, interesovanjePath);
+            return i;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lose interesovanje");
+        }
+    }
+
+    @Override
+    public Interesovanje dobaviInteresovanje(String id) {
+        var interesovanje = this.findById(id);
+        if (interesovanje.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Interesovanje nije pronadjeno");
+        }
+        return interesovanje.get();
     }
 
 }
--- vaccination-portal/src/main/java/com/tim123/vaccinationportal/util/Constants.java
+++ vaccination-portal/src/main/java/com/tim123/vaccinationportal/util/Constants.java
@@ -3,5 +3,6 @@ package com.tim123.vaccinationportal.util;
 public class Constants {
     private static final String base = "http://www.xws.org";
     public static final String interesovanjeBase = String.format("%s/interesovanje", base);
+    public static final String interesovanjePath = "/fuseki/interesovanje";
     public static final String interesovanjeCollection = "db/vakcinisanje/interesovanje";
 }
