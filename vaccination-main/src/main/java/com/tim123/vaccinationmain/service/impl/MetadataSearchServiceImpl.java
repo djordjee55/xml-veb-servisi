2e0604aa48fdece77ef9f74280abdf2141ab4dbc
deksa99 <deja99@live.com>
2022-07-12 06:57:12 +0200

    query parser

--- vaccination-main/src/main/java/com/tim123/vaccinationmain/service/impl/MetadataSearchServiceImpl.java
+++ vaccination-main/src/main/java/com/tim123/vaccinationmain/service/impl/MetadataSearchServiceImpl.java
@@ -9,17 +9,49 @@ import org.springframework.stereotype.Service;
 public class MetadataSearchServiceImpl implements MetadataSearchService {
     @Override
     public ListaDokumenata search(TipDokumenta type, String query) {
-        // Parsiraj query - ima isti oblik kao filter, exp se samo menjaju na osnovu tipa
-            // Moze da se radi o datumu
-            // Literalu
-            // String vrednosti (contains lowercase)
-        // 1. Specijalnie karaktere ( ) || && ubacuj u izraz
-        // 2. Kada naidjes na '^' kreni da vadis exp koji se nalazi do sledeceg '^'
-        // 3. Obradi taj exp (kreiraj odgovarajuci sparql upit) na osnovu naziva metapodatka po kome se filtrira
+        String parsedQuery = parseQuery(type, query);
         // Nadji o kom se dokumentu radi
         // Za dati dokument napravi filter na osnovu sablona - imenuj objekte (poklapa se sa nazivom) za date predikate
         // Uputi zahtev ka bazi
         // Vrati rezultat
         return new ListaDokumenata();
+    }
+
+    private String parseQuery(TipDokumenta type, String query) {
+        // query=^name=value^||(^name=value^&&^name=value^||(^name=value^&&^name=value^))
+
+        // Parsiraj query - ima isti oblik kao filter, exp se samo menjaju na osnovu tipa
+        StringBuilder mdFilter = new StringBuilder();
+        // Ukloni whitespace karaktere
+        query = query.replaceAll("\\s", "");
+        query = query.strip();
+        int idx = 0;
+        while (idx < query.length()) {
+            var c = query.charAt(idx);
+            // Dozvoli query sa praznim karakterima
+            if (c == ' ') {
+                continue;
+            }
+            // Specijalnie karaktere ( ) || && ubacuj u izraz
+            else if (c == '(' || c == ')' || c == '|' || c == '&') {
+                mdFilter.append(c);
+            }
+            // Kada naidjes na '^' kreni da vadis exp koji se nalazi do sledeceg '^'
+            else if (c == '^') {
+                var end = query.indexOf('^', idx + 1);
+                var exp = query.substring(idx + 1, end);
+                // Obradi izraz tako da kreira oblik pogodan za sparql upit i dodaj u filter
+                mdFilter.append(parseExp(type, exp));
+                // Preskoci do kraja izraza
+                idx = end;
+            }
+            ++idx;
+        }
+        return mdFilter.toString();
+    }
+
+    private String parseExp(TipDokumenta type, String exp) {
+        // TODO
+        return exp;
     }
 }
