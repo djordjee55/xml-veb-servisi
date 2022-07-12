bf37a4dd98a2168a73990c52ed43feff188b9b81
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-07-12 17:07:54 +0200

    Url changed.

--- vaccination-main/src/main/java/com/tim123/vaccinationmain/controller/MetadataSearchController.java
+++ vaccination-main/src/main/java/com/tim123/vaccinationmain/controller/MetadataSearchController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/metadata-search")
+@RequestMapping("/api/metadata-search")
 @RequiredArgsConstructor
 public class MetadataSearchController {
 
     private final MetadataSearchService metadataSearchService;
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_XML_VALUE)
+    @GetMapping(value = "", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<ListaDokumenata> search(@RequestParam TipDokumenta type, @RequestParam String query) {
         var result = metadataSearchService.search(type, query);
         return ResponseEntity.ok(result);
